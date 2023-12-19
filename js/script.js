00143f7052343795c4388ac271c1a27c3664fe4e
nganDayat <elhidayat000@gmail.com>
2023-12-19 14:20:05 +0700

    update

--- js/script.js
+++ js/script.js
@@ -3,16 +3,46 @@ window.addEventListener("scroll", function name() {
     header.classList.toggle ("sticky", window.scrollY > 0);
 });
 
-let menu=document.querySelector("#menu-icon");
-let navbar=document.querySelector(".navbar");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
-menu.onclick=() => {
-    menu.classList.toggle("bx-x");
+menu.onclick = () => {
+    menu.classList.toggle("bx-x-circle");
     navbar.classList.toggle("active");
 }
 
+window.onscroll = () =>{
+    menu.classList.remove("bx-x-circle");
+    navbar.classList.remove("active");
+}
 
 
+// membuat animasi saat elemen masuk ke dalam viewport
+// membuat ojeck bru pd scrollreveal, lalu konfigurasi
+const sr = ScrollReveal ({
+    distance: "25px",
+    duration: 2500,
+    reset: true
+})
 
+// memberikan animasi pada bagian2 utama
+sr.reveal(".home-text", {delay:210, origin:"bottom"})
+sr.reveal(".about, .skill, .project, .contact", {delay:220, origin:"bottom"})
+
+$(document).ready(function () {
+    $(".send").click(function (e) {
+        e.preventDefault();
+
+        setTimeout(function() {
+            $("#submit-message").text("Message sent successfully").addClass("success-message");
+        
+            $(".contact-form form")[0].reset();
+
+            setTimeout(function() {
+                $("#submit-message").text("").removeClass("success-message");
+            }, 3000);
+    }, 1000);
+    });
+});
 
 
